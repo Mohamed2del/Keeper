c3d40dfb97a1eb36ca13fd430248c8ff7c5489e9
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2020-06-15 04:26:21 +0200

    fixing

---
+++ build/precache-manifest.604d80f1ea96cc7b7909b5144acff1a8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d5f17d7bd6dd87bec6c3c5d9a25694",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "076cc373a1b027319c02",
+    "url": "/Keeper/static/js/2.b30f0d40.chunk.js"
+  },
+  {
+    "revision": "4784dece783179da6a28",
+    "url": "/Keeper/static/js/main.3ceba09c.chunk.js"
+  },
+  {
+    "revision": "fafb5b73cfe8532f5c2a",
+    "url": "/Keeper/static/js/runtime-main.01f0de92.js"
+  }
+]);
